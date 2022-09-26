65c01764baa893d8558fff2135be875c2a75c8d7
nandirchrd <nandirchrd@gmail.com>
2022-09-26 21:56:47 +0700

    rename App to app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Footer, Navbar } from './core-ui';
+import { Navbar } from './core-ui';
 import { Home, Blogs, Blog, Photos, Photo } from './pages';
-import { ThemeProvider } from './contexts/themeContext';
-import { ActivePageProvider } from './contexts/activePageContext';
 
 function App() {
 	useEffect(() => {
 		// Comp Did Mount
 	}, []);
 	return (
-		<ActivePageProvider>
-			<ThemeProvider>
-				{/* NAVBAR */}
-				<Navbar />
-				<Routes>
-					<Route index path='/' element={<Home />} />
-					<Route index path='/blogs' element={<Blogs />} />
-					<Route index path='/blogs/:slug' element={<Blog />} />
-					<Route index path='/photos' element={<Photos />} />
-					<Route index path='/photos/:slug' element={<Photo />} />
-					<Route index path='*' element={<Navigate to='/' />} />
-				</Routes>
-				<Footer />
-			</ThemeProvider>
-		</ActivePageProvider>
+		<>
+			{/* NAVBAR */}
+			<Navbar />
+			<Routes>
+				<Route index path='/' element={<Home />} />
+				<Route index path='/blogs' element={<Blogs />} />
+				<Route index path='/blogs/:slug' element={<Blog />} />
+				<Route index path='/photos' element={<Photos />} />
+				<Route index path='/photos/:slug' element={<Photo />} />
+				<Route index path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</>
 	);
 }
 
