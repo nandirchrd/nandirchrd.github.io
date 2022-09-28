af36ec39f47b98f5fdcef992f616ab602a6bdded
nandirchrd <nandirchrd@gmail.com>
2022-09-28 18:28:53 +0700

    Fixed navigate when blogs slug doesn't match

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import { getBlog } from '../services/blogService';
 import BlockContent from '@sanity/block-content-to-react';
 
 const Blog = () => {
 	const { slug } = useParams();
-	const [blog, setBlog] = useState(null);
+	const [blog] = useState(getBlog(slug));
 
-	useState(() => {
-		const blog = getBlog(slug);
-		setBlog(blog);
-	}, []);
+	if (!blog) {
+		return <Navigate to='/blogs' />;
+	}
+
 	return (
 		<main id='blog' className='mt-20 pb-10'>
-			{blog && (
-				<section>
-					<article className='container mx-auto '>
-						<img
-							className='aspect-video w-full max-h-[25em] rounded-md'
-							src={blog.thumb}
-							alt={blog.title}
-						/>
-						<h1 className='text-4xl font-bold mt-2'>
-							{blog.title}
-						</h1>
-						<div className='body mt-2'>
-							<BlockContent blocks={blog.body} />
-						</div>
-					</article>
-				</section>
-			)}
+			<section>
+				<article className='container mx-auto '>
+					<img
+						className='aspect-video w-full max-h-[25em] rounded-md'
+						src={blog.thumb}
+						alt={blog.title}
+					/>
+					<h1 className='text-4xl font-bold mt-2'>{blog.title}</h1>
+					<div className='body mt-2'>
+						<BlockContent blocks={blog.body} />
+					</div>
+				</article>
+			</section>
 		</main>
 	);
 };
